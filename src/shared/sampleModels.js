60a4e4230f0cae5744aff8a48f5e5b39f9426626
manoj kumar <Manojgum@gmail.com>
2023-06-01 14:20:18 +0530

    Update sampleModels.js

--- src/shared/sampleModels.js
+++ src/shared/sampleModels.js
@@ -105,13 +105,14 @@ function messageButtons(faq, number) {
 }
 
 async function generateListTemplateRow(data) {
-    const rows = data.map(row => ({
-      title: row.title,
-      description: row.description,
-      _id: row._id
-    }));
-  
-    return rows;
+    console.log("data......",data)
+    const rows = data.map(row =>{
+        "title"= row.title,
+        "description"= row.description,
+       " id"= row._id
+    });
+    
+      return rows;
   }
   
 async function messageList(faq, number){
@@ -119,21 +120,21 @@ async function messageList(faq, number){
     const rows = await generateListTemplateRow(faq.list.buttonslist);
     console.log({"rows":rows})
 
-    const data = {
-        messaging_product: "whatsapp",
-        to: number,
-        type: "interactive",
-        interactive: {
-            type: "list",
-            body: {
-                text: faq.list.responsetext
+    const data = JSON.stringify({
+        "messaging_product": "whatsapp",
+        "to": number,
+        "type": "interactive",
+        "interactive": {
+            "type": "list",
+            "body": {
+                "text": faq.list.responsetext
             },
-            action: {
-                button: "Select Option",
-                sections:     [
+            "action": {
+                "button": "Select Option",
+                "sections":     [
                     {
-                        title: "LIST_SECTION_1_TITLE",
-                        rows: rows
+                        "title": "LIST_SECTION_1_TITLE",
+                        "rows": rows
                     }
                 ]
 
@@ -166,8 +167,7 @@ async function messageList(faq, number){
                 // })
             }
         }
-    };
-    console.log("data---------data",data)
+    });
     return data;
 }
 
