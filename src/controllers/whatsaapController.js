a9fcc662397ef7bcd1871f452ce9281b246e4df2
manoj kumar <Manojgum@gmail.com>
2023-06-15 11:46:08 +0530

    Update whatsaapController.js

--- src/controllers/whatsaapController.js
+++ src/controllers/whatsaapController.js
@@ -56,14 +56,14 @@ const receivedMessage = async (req, res) => {
       console.log("user request text.......", text)
 
       let maxSimilarity = 0;
-      const similarityThreshold = 0.6;
+      let similarityThreshold = 0.6;
       for (let i = 0; i < data.length; i++) {
         var faq = data[i];
         const similarity = await getJaccardSimilarity(
           text.toLowerCase(),
           faq.question.toLowerCase()
         );
-        if (similarity >= 0.6 && similarity > maxSimilarity) {
+        if (similarity >= similarityThreshold && similarity > maxSimilarity) {
           const infoType = faq.infoType.infoType.toLowerCase()
           console.log("type Info.......", infoType);
           if (infoType === "text") {
