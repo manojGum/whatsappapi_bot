2683d5d11442ce6da7fa500a876968870b1ac5d4
manoj kumar <Manojgum@gmail.com>
2023-06-23 15:35:45 +0530

    optmize whatsapp service.js

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -1,39 +1,66 @@
-const https = require("https")
 
-function sendMessageWhatsApp(data){
-    // /*
-    // const data = JSON.stringify({
-    //     "messaging_product": "whatsapp",    
-    //     "to": number,
-    //     "type": "text",
-    //     "text": {
-    //         "body": textResponse
-    //     }
-    // });
-    // */
-    const options = {
-        host:"graph.facebook.com",
-        path:"/v16.0/122432790850913/messages",
-        method:"POST",
-        body:data,
-        headers:{
-            "Content-Type":"application/json",
-            Authorization:`Bearer ${process.env.WHATSAPP_CLOUD_API_VERIFICATION_TOKEN}`
-        }
-    };
-    const req = https.request(options,res=>{
-        res.on('data',d=>{
-            process.stdout.write(d);
-        });
-    });
+// const https = require("https")
 
-    req.on("error",error=>{
-        console.error(error);
+// function sendMessageWhatsApp(data){
+//     // /*
+//     // const data = JSON.stringify({
+//     //     "messaging_product": "whatsapp",    
+//     //     "to": number,
+//     //     "type": "text",
+//     //     "text": {
+//     //         "body": textResponse
+//     //     }
+//     // });
+//     // */
+//     const options = {
+//         host:"graph.facebook.com",
+//         path:"/v16.0/122432790850913/messages",
+//         method:"POST",
+//         body:data,
+//         headers:{
+//             "Content-Type":"application/json",
+//             Authorization:`Bearer ${process.env.WHATSAPP_CLOUD_API_VERIFICATION_TOKEN}`
+//         }
+//     };
+//     const req = https.request(options,res=>{
+//         res.on('data',d=>{
+//             process.stdout.write(d);
+//         });
+//     });
+
+//     req.on("error",error=>{
+//         console.error(error);
+//     });
+//     req.write(data);
+//     req.end();
+// }
+
+// module.exports ={
+//     sendMessageWhatsApp
+// }
+
+const axios = require("axios");
+
+function sendMessageWhatsApp(data) {
+  const options = {
+    url: "https://graph.facebook.com/v16.0/122432790850913/messages",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.WHATSAPP_CLOUD_API_VERIFICATION_TOKEN}`,
+    },
+    data: data,
+  };
+
+  axios(options)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
     });
-    req.write(data);
-    req.end();
 }
 
-module.exports ={
-    sendMessageWhatsApp
-}
+module.exports = {
+  sendMessageWhatsApp,
+};
